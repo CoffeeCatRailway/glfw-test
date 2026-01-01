cca3bfbb505e89ec9bebc5a026f34f0d9cca068e
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-01-01 11:49:51 +1100

    format and shader strings

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,14 @@ use crate::shader::Shader;
 
 use imgui::Context as ImContext;
 use imgui_glfw_rs::ImguiGLFW;
-use imgui_glfw_rs::glfw::{Context, Key, Action};
+use imgui_glfw_rs::glfw::{Action, Context, Key};
 use imgui_glfw_rs::imgui as ImGui;
 
 use gl;
 use gl::types::*;
 
-use std::ffi::{CStr};
-use std::{mem, ptr};
 use std::os::raw::c_void;
-
-macro_rules! c_str {
-    ($literal:expr) => {
-        CStr::from_bytes_with_nul_unchecked(concat!($literal, "\0").as_bytes())
-    }
-}
+use std::{mem, ptr};
 
 // settings
 const SCR_WIDTH: u32 = 800;
@@ -28,150 +21,172 @@ const SCR_HEIGHT: u32 = 600;
 fn main() {
     println!("Hello, world!");
 
-	let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
-	glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
-	glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-	#[cfg(target_os = "macos")] // Whoever uses mac for some reason
-	glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    #[cfg(target_os = "macos")] // Whoever uses mac for some reason
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
-	let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "Hello, world!", glfw::WindowMode::Windowed)
-		.expect("Failed to create GLFW window.");
+    let (mut window, events) = glfw
+        .create_window(
+            SCR_WIDTH,
+            SCR_HEIGHT,
+            "Hello, world!",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window.");
 
-	window.make_current();
-	window.set_all_polling(true);
+    window.make_current();
+    window.set_all_polling(true);
 
-	// gl: load all OpenGL function pointers
-	gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+    // gl: load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-	let (shader, VAO, VBO, EBO, elementCount) = unsafe {
-		let shader = Shader::new("resources/shaders/vertex.vert", "resources/shaders/fragment.frag");
+    let (shader, VAO, VBO, EBO, elementCount) = unsafe {
+        let shader = Shader::new(
+            "resources/shaders/vertex.vert",
+            "resources/shaders/fragment.frag",
+        );
 
-		// vertex data and vao
-		// let vertices: [f32; 12] = [
-		// 	 0.5,  0.5,  0.0, // top right
-		// 	 0.5, -0.5,  0.0, // bottom right
-		// 	-0.5, -0.5, -0.5, // bottom left
-		// 	-0.5,  0.5,  0.0, // top left
-		// ];
-		// let indices = [
-		// 	0, 1, 3,
-		// 	1, 2, 3
-		// ];
-		let vertices: [f32; 24] = [
-			-0.5,  0.5,  0.5, // top left
-			 0.0,  0.5,  0.0, // top
+        // vertex data and vao
+        // let vertices: [f32; 12] = [
+        // 	 0.5,  0.5,  0.0, // top right
+        // 	 0.5, -0.5,  0.0, // bottom right
+        // 	-0.5, -0.5, -0.5, // bottom left
+        // 	-0.5,  0.5,  0.0, // top left
+        // ];
+        // let indices = [
+        // 	0, 1, 3,
+        // 	1, 2, 3
+        // ];
+        let vertices: [f32; 24] = [
+            -0.5, 0.5, 0.5, // top left
+            0.0, 0.5, 0.0, // top
+            0.5, 0.5, 0.5, // top right
+            0.5, 0.0, 0.0, // right
+            0.5, -0.5, 0.5, // bottom right
+            0.0, -0.5, 0.0, // bottom
+            -0.5, -0.5, 0.5, // bottom left
+            -0.5, 0.0, 0.0, // left
+        ];
+        let indices = [
+            0, 1, 7, // top left
+            1, 2, 3, // top right
+            3, 4, 5, // bottom right
+            7, 5, 6, // bottom left
+            1, 3, 5, 1, 5, 7,
+        ];
+        let (mut VAO, mut VBO, mut EBO) = (0, 0, 0);
+        gl::GenVertexArrays(1, &mut VAO);
+        gl::GenBuffers(1, &mut VBO);
+        gl::GenBuffers(1, &mut EBO);
+        gl::BindVertexArray(VAO);
 
-			 0.5,  0.5,  0.5, // top right
-			 0.5,  0.0,  0.0, // right
+        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            vertices.as_ptr() as *const c_void,
+            gl::STATIC_DRAW,
+        );
 
-			 0.5, -0.5,  0.5, // bottom right
-			 0.0, -0.5,  0.0, // bottom
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, EBO);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            indices.as_ptr() as *const c_void,
+            gl::STATIC_DRAW,
+        );
 
-			-0.5, -0.5,  0.5, // bottom left
-			-0.5,  0.0,  0.0, // left
-		];
-		let indices = [
-			0, 1, 7, // top left
-			1, 2, 3, // top right
-			3, 4, 5, // bottom right
-			7, 5, 6, // bottom left
-			1, 3, 5,
-			1, 5, 7,
-		];
-		let (mut VAO, mut VBO, mut EBO) = (0, 0, 0);
-		gl::GenVertexArrays(1, &mut VAO);
-		gl::GenBuffers(1, &mut VBO);
-		gl::GenBuffers(1, &mut EBO);
-		gl::BindVertexArray(VAO);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * mem::size_of::<GLfloat>() as GLsizei,
+            ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
 
-		gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
-		gl::BufferData(gl::ARRAY_BUFFER,
-					   (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-					   vertices.as_ptr() as *const c_void,
-					   gl::STATIC_DRAW);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
 
-		gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, EBO);
-		gl::BufferData(gl::ELEMENT_ARRAY_BUFFER,
-					   (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-					   indices.as_ptr() as *const c_void,
-					   gl::STATIC_DRAW);
+        (shader, VAO, VBO, EBO, indices.len() as GLsizei)
+    };
 
-		gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
-		gl::EnableVertexAttribArray(0);
+    let mut imgui = ImContext::create();
+    imgui.set_ini_filename(None);
+    imgui.set_log_filename(None);
+    let mut imguiGlfw = ImguiGLFW::new(&mut imgui, &mut window);
+    imgui.io_mut().display_size = [SCR_WIDTH as f32, SCR_HEIGHT as f32];
+    imgui.io_mut().display_framebuffer_scale = [1.0, 1.0];
 
-		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-		gl::BindVertexArray(0);
+    while !window.should_close() {
+        // update
 
-		(shader, VAO, VBO, EBO, indices.len() as GLsizei)
-	};
+        // render
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
 
-	let mut imgui = ImContext::create();
-	imgui.set_ini_filename(None);
-	imgui.set_log_filename(None);
-	let mut imguiGlfw = ImguiGLFW::new(&mut imgui, &mut window);
-	imgui.io_mut().display_size = [SCR_WIDTH as f32, SCR_HEIGHT as f32];
-	imgui.io_mut().display_framebuffer_scale = [1.0, 1.0];
+            shader.bind();
 
-	while !window.should_close() {
-		// update
+            let time = glfw.get_time() as f32;
 
-		// render
-		unsafe {
-			gl::ClearColor(0.2, 0.3, 0.3, 1.0);
-			gl::Clear(gl::COLOR_BUFFER_BIT);
+            let third = 1.0f32 / 3.0;
+            // abs(mod(x,2)-1) = triangle wave
 
-			shader.bind();
+            let red = ((0.5 * time) % 2.0 - 1.0).abs(); //time.cos() * 0.5 + 0.5;
+            let green = ((0.5 * time + third) % 2.0 - 1.0).abs(); //time.sin() * 0.5 + 0.5;
+            let blue = ((0.5 * time - third) % 2.0 - 1.0).abs();
+            shader.setUniform3f("u_color", red, green, blue);
 
-			let time = glfw.get_time() as f32;
+            gl::BindVertexArray(VAO);
+            gl::DrawElements(gl::TRIANGLES, elementCount, gl::UNSIGNED_INT, ptr::null());
 
-			let third = 1.0f32 / 3.0;
-			// abs(mod(x,2)-1) = triangle wave
+            let error = gl::GetError();
+            if error != gl::NO_ERROR {
+                panic!("OpenGL error ({})", error);
+            }
+        }
 
-			let red = ((0.5 * time) % 2.0 - 1.0).abs();//time.cos() * 0.5 + 0.5;
-			let green = ((0.5 * time + third) % 2.0 - 1.0).abs();//time.sin() * 0.5 + 0.5;
-			let blue = ((0.5 * time - third) % 2.0 - 1.0).abs();
-			shader.setUniform3f(c_str!("u_color"), red, green, blue);
+        let ui = imguiGlfw.frame(&mut window, &mut imgui);
+        // ui.show_demo_window(&mut true);
+        ui.window("ye")
+            .size([100.0, 100.0], ImGui::Condition::FirstUseEver)
+            .build(|| {
+                ui.text("Hello, world!".to_string());
+                let s = ui.window_size();
+                ui.text(format!("{}/{}", s[0], s[1]));
+            });
+        imguiGlfw.draw(&mut imgui, &mut window);
 
-			gl::BindVertexArray(VAO);
-			gl::DrawElements(gl::TRIANGLES, elementCount, gl::UNSIGNED_INT, ptr::null());
+        window.swap_buffers();
+        glfw.poll_events();
 
-			let error = gl::GetError();
-			if error != gl::NO_ERROR {
-				panic!("OpenGL error ({})", error);
-			}
-		}
+        // events
+        for (_, event) in glfw::flush_messages(&events) {
+            imguiGlfw.handle_event(&mut imgui, &event);
+            match event {
+                glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                    gl::Viewport(0, 0, width, height)
+                },
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
 
-		let ui = imguiGlfw.frame(&mut window, &mut imgui);
-		// ui.show_demo_window(&mut true);
-		ui.window("ye").size([100.0, 100.0], ImGui::Condition::FirstUseEver).build(|| {
-			ui.text("Hello, world!".to_string());
-		});
-		imguiGlfw.draw(&mut imgui, &mut window);
-
-		window.swap_buffers();
-		glfw.poll_events();
-
-		// events
-		for (_, event) in glfw::flush_messages(&events) {
-			imguiGlfw.handle_event(&mut imgui, &event);
-			match event {
-				glfw::WindowEvent::FramebufferSize(width, height) => {
-					unsafe { gl::Viewport(0, 0, width, height) }
-				}
-				glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-					window.set_should_close(true)
-				}
-				_ => {}
-			}
-		}
-	}
-
-	unsafe {
-		shader.delete();
-		gl::DeleteBuffers(1, &EBO);
-		gl::DeleteBuffers(1, &VBO);
-		gl::DeleteVertexArrays(1, &VAO);
-	}
+    unsafe {
+        shader.delete();
+        gl::DeleteBuffers(1, &EBO);
+        gl::DeleteBuffers(1, &VBO);
+        gl::DeleteVertexArrays(1, &VAO);
+    }
 }
 
 // fn handle_window_event(window: &mut glfw::Window, events: &GlfwReceiver<(f64, glfw::WindowEvent)>) {
--- src/shader.rs
+++ src/shader.rs
@@ -4,166 +4,228 @@ use cgmath::{Matrix, Matrix4, Vector2, Vector3, Vector4};
 use gl;
 use gl::types::*;
 
-use std::ffi::{CStr, CString};
+use std::ffi::CString;
 use std::fs::File;
 use std::io::Read;
 use std::ptr;
 use std::str;
 
 pub struct Shader {
-	pub id: u32,
+    pub id: u32,
 }
 
 #[allow(dead_code)]
 impl Shader {
-	pub fn new(vertexPath: &str, fragmentPath: &str) -> Shader {
-		let mut shader = Shader { id: 0 };
-		unsafe {
-			let vertex = shader.compileShader(vertexPath, gl::VERTEX_SHADER);
-			let fragment = shader.compileShader(fragmentPath, gl::FRAGMENT_SHADER);
+    pub fn new(vertexPath: &str, fragmentPath: &str) -> Shader {
+        let mut shader = Shader { id: 0 };
+        unsafe {
+            let vertex = shader.compileShader(vertexPath, gl::VERTEX_SHADER);
+            let fragment = shader.compileShader(fragmentPath, gl::FRAGMENT_SHADER);
 
-			let id = gl::CreateProgram();
-			gl::AttachShader(id, vertex);
-			gl::AttachShader(id, fragment);
+            let id = gl::CreateProgram();
+            gl::AttachShader(id, vertex);
+            gl::AttachShader(id, fragment);
 
-			let fragColor = CString::new("o_color").unwrap();
-			gl::BindFragDataLocation(id, 0, fragColor.as_ptr());
-			gl::LinkProgram(id);
+            let fragColor = CString::new("o_color").unwrap();
+            gl::BindFragDataLocation(id, 0, fragColor.as_ptr());
+            gl::LinkProgram(id);
 
-			shader.checkLinkStatus(id);
+            shader.checkLinkStatus(id);
 
-			gl::DeleteShader(vertex);
-			gl::DeleteShader(fragment);
-			shader.id = id;
-		}
-		shader
-	}
+            gl::DeleteShader(vertex);
+            gl::DeleteShader(fragment);
+            shader.id = id;
+        }
+        shader
+    }
 
-	pub fn newWithGeometry(vertexPath: &str, geometryPath: &str, fragmentPath: &str) -> Shader {
-		let mut shader = Shader { id: 0 };
-		unsafe {
-			let vertex = shader.compileShader(vertexPath, gl::VERTEX_SHADER);
-			let geometry = shader.compileShader(geometryPath, gl::GEOMETRY_SHADER);
-			let fragment = shader.compileShader(fragmentPath, gl::FRAGMENT_SHADER);
+    pub fn newWithGeometry(vertexPath: &str, geometryPath: &str, fragmentPath: &str) -> Shader {
+        let mut shader = Shader { id: 0 };
+        unsafe {
+            let vertex = shader.compileShader(vertexPath, gl::VERTEX_SHADER);
+            let geometry = shader.compileShader(geometryPath, gl::GEOMETRY_SHADER);
+            let fragment = shader.compileShader(fragmentPath, gl::FRAGMENT_SHADER);
 
-			let id = gl::CreateProgram();
-			gl::AttachShader(id, vertex);
-			gl::AttachShader(id, geometry);
-			gl::AttachShader(id, fragment);
+            let id = gl::CreateProgram();
+            gl::AttachShader(id, vertex);
+            gl::AttachShader(id, geometry);
+            gl::AttachShader(id, fragment);
 
-			let fragColor = CString::new("o_color").unwrap();
-			gl::BindFragDataLocation(id, 0, fragColor.as_ptr());
-			gl::LinkProgram(id);
+            let fragColor = CString::new("o_color").unwrap();
+            gl::BindFragDataLocation(id, 0, fragColor.as_ptr());
+            gl::LinkProgram(id);
 
-			shader.checkLinkStatus(id);
+            shader.checkLinkStatus(id);
 
-			gl::DeleteShader(vertex);
-			gl::DeleteShader(geometry);
-			gl::DeleteShader(fragment);
-			shader.id = id;
-		}
-		shader
-	}
+            gl::DeleteShader(vertex);
+            gl::DeleteShader(geometry);
+            gl::DeleteShader(fragment);
+            shader.id = id;
+        }
+        shader
+    }
 
-	pub unsafe fn bind(&self) {
-		unsafe { gl::UseProgram(self.id); }
-	}
+    pub unsafe fn bind(&self) {
+        unsafe {
+            gl::UseProgram(self.id);
+        }
+    }
 
-	pub unsafe fn delete(&self) {
-		unsafe { gl::DeleteShader(self.id); }
-	}
+    pub unsafe fn delete(&self) {
+        unsafe {
+            gl::DeleteShader(self.id);
+        }
+    }
 
-	pub unsafe fn getAttribLocation(&self, name: &CStr) -> GLint {
-		unsafe { gl::GetAttribLocation(self.id, name.as_ptr()) }
-	}
+    pub unsafe fn getAttribLocation(&self, name: &str) -> GLint {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::GetAttribLocation(self.id, c_str.as_ptr() as *const GLchar)
+        }
+    }
 
-	// Uniforms
-	pub unsafe fn setUniform1i(&self, name: &CStr, value: i32) {
-		unsafe { gl::Uniform1i(gl::GetUniformLocation(self.id, name.as_ptr()), value); }
-	}
+    // Uniforms
+    pub unsafe fn setUniform1i(&self, name: &str, value: i32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform1i(
+                gl::GetUniformLocation(self.id, c_str.as_ptr() as *const GLchar),
+                value,
+            );
+        }
+    }
 
-	pub unsafe fn setUniform1ui(&self, name: &CStr, value: u32) {
-		unsafe { gl::Uniform1ui(gl::GetUniformLocation(self.id, name.as_ptr()), value); }
-	}
+    pub unsafe fn setUniform1ui(&self, name: &str, value: u32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform1ui(gl::GetUniformLocation(self.id, c_str.as_ptr()), value);
+        }
+    }
 
-	pub unsafe fn setUniform1f(&self, name: &CStr, value: f32) {
-		unsafe { gl::Uniform1f(gl::GetUniformLocation(self.id, name.as_ptr()), value); }
-	}
+    pub unsafe fn setUniform1f(&self, name: &str, value: f32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform1f(gl::GetUniformLocation(self.id, c_str.as_ptr()), value);
+        }
+    }
 
-	pub unsafe fn setUniform2fv(&self, name: &CStr, value: &Vector2<f32>) {
-		unsafe { self.setUniform2f(name, value.x, value.y); }
-	}
+    pub unsafe fn setUniform2fv(&self, name: &str, value: &Vector2<f32>) {
+        unsafe {
+            self.setUniform2f(name, value.x, value.y);
+        }
+    }
 
-	pub unsafe fn setUniform2f(&self, name: &CStr, x: f32, y: f32) {
-		unsafe { gl::Uniform2f(gl::GetUniformLocation(self.id, name.as_ptr()), x, y); }
-	}
+    pub unsafe fn setUniform2f(&self, name: &str, x: f32, y: f32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform2f(gl::GetUniformLocation(self.id, c_str.as_ptr()), x, y);
+        }
+    }
 
-	pub unsafe fn setUniform3fv(&self, name: &CStr, value: &Vector3<f32>) {
-		unsafe { self.setUniform3f(name, value.x, value.y, value.z); }
-	}
+    pub unsafe fn setUniform3fv(&self, name: &str, value: &Vector3<f32>) {
+        unsafe {
+            self.setUniform3f(name, value.x, value.y, value.z);
+        }
+    }
 
-	pub unsafe fn setUniform3f(&self, name: &CStr, x: f32, y: f32, z: f32) {
-		unsafe { gl::Uniform3f(gl::GetUniformLocation(self.id, name.as_ptr()), x, y, z); }
-	}
+    pub unsafe fn setUniform3f(&self, name: &str, x: f32, y: f32, z: f32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform3f(gl::GetUniformLocation(self.id, c_str.as_ptr()), x, y, z);
+        }
+    }
 
-	pub unsafe fn setUniform4fv(&self, name: &CStr, value: &Vector4<f32>) {
-		unsafe { self.setUniform4f(name, value.x, value.y, value.z, value.w); }
-	}
+    pub unsafe fn setUniform4fv(&self, name: &str, value: &Vector4<f32>) {
+        unsafe {
+            self.setUniform4f(name, value.x, value.y, value.z, value.w);
+        }
+    }
 
-	pub unsafe fn setUniform4f(&self, name: &CStr, x: f32, y: f32, z: f32, w: f32) {
-		unsafe { gl::Uniform4f(gl::GetUniformLocation(self.id, name.as_ptr()), x, y, z, w); }
-	}
+    pub unsafe fn setUniform4f(&self, name: &str, x: f32, y: f32, z: f32, w: f32) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::Uniform4f(gl::GetUniformLocation(self.id, c_str.as_ptr()), x, y, z, w);
+        }
+    }
 
-	pub unsafe fn setMatrix4f(&self, name: &CStr, mat: &Matrix4<f32>) {
-		unsafe { gl::UniformMatrix4fv(gl::GetUniformLocation(self.id, name.as_ptr()), 1, gl::FALSE, mat.as_ptr()); }
-	}
+    pub unsafe fn setMatrix4f(&self, name: &str, mat: &Matrix4<f32>) {
+        unsafe {
+            let c_str = CString::new(name.as_bytes()).unwrap();
+            gl::UniformMatrix4fv(
+                gl::GetUniformLocation(self.id, c_str.as_ptr()),
+                1,
+                gl::FALSE,
+                mat.as_ptr(),
+            );
+        }
+    }
 
-	// Utility
-	unsafe fn compileShader(&self, path: &str, shaderType: GLenum) -> GLuint {
-		let mut file = File::open(path).unwrap_or_else(|_| panic!("Failed to open {}", path));
-		let mut code = String::new();
-		file.read_to_string(&mut code).expect("Failed to read vertex shader");
-		let source = CString::new(code.as_bytes()).unwrap();
+    // Utility
+    unsafe fn compileShader(&self, path: &str, shaderType: GLenum) -> GLuint {
+        let mut file = File::open(path).unwrap_or_else(|_| panic!("Failed to open {}", path));
+        let mut code = String::new();
+        file.read_to_string(&mut code)
+            .expect("Failed to read vertex shader");
+        let source = CString::new(code.as_bytes()).unwrap();
 
-		unsafe {
-			let shader = gl::CreateShader(shaderType);
+        unsafe {
+            let shader = gl::CreateShader(shaderType);
 
-			// Attempt compile
-			gl::ShaderSource(shader, 1, &source.as_ptr(), ptr::null());
-			gl::CompileShader(shader);
+            // Attempt compile
+            gl::ShaderSource(shader, 1, &source.as_ptr(), ptr::null());
+            gl::CompileShader(shader);
 
-			// Get compile status
-			let mut status = gl::FALSE as GLint;
-			gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+            // Get compile status
+            let mut status = gl::FALSE as GLint;
+            gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
 
-			// Check if failed
-			if status != (gl::TRUE as GLint) {
-				let mut len = 0;
-				gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
-				let mut buf = Vec::with_capacity(len as usize);
-				buf.set_len((len as usize) - 1);
-				gl::GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-				println!("Shader compilation error:\n{}", str::from_utf8(&buf).ok().expect("ShaderInfoLog not valid utf8"));
-			} else { println!("Shader compilation successful: {}-{}", shader, path); }
+            // Check if failed
+            if status != (gl::TRUE as GLint) {
+                let mut len = 0;
+                gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1);
+                gl::GetShaderInfoLog(
+                    shader,
+                    len,
+                    ptr::null_mut(),
+                    buf.as_mut_ptr() as *mut GLchar,
+                );
+                println!(
+                    "Shader compilation error:\n{}",
+                    str::from_utf8(&buf)
+                        .ok()
+                        .expect("ShaderInfoLog not valid utf8")
+                );
+            } else {
+                println!("Shader compilation successful: {}-{}", shader, path);
+            }
 
-			shader
-		}
-	}
+            shader
+        }
+    }
 
-	unsafe fn checkLinkStatus(&self, id: GLuint) {
-		unsafe {
-			let mut status = gl::FALSE as GLint;
-			gl::GetProgramiv(id, gl::LINK_STATUS, &mut status);
+    unsafe fn checkLinkStatus(&self, id: GLuint) {
+        unsafe {
+            let mut status = gl::FALSE as GLint;
+            gl::GetProgramiv(id, gl::LINK_STATUS, &mut status);
 
-			// Check if failed
-			if status != (gl::TRUE as GLint) {
-				let mut len: GLint = 0;
-				gl::GetProgramiv(id, gl::INFO_LOG_LENGTH, &mut len);
-				let mut buf = Vec::with_capacity(len as usize);
-				buf.set_len((len as usize) - 1);
-				gl::GetProgramInfoLog(id, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-				println!("Shader linking error:\n{}", str::from_utf8(&buf).ok().expect("ProgramInfoLog not valid utf8"));
-			} else { println!("Shader linking successful: {}", id); }
-		}
-	}
+            // Check if failed
+            if status != (gl::TRUE as GLint) {
+                let mut len: GLint = 0;
+                gl::GetProgramiv(id, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1);
+                gl::GetProgramInfoLog(id, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+                println!(
+                    "Shader linking error:\n{}",
+                    str::from_utf8(&buf)
+                        .ok()
+                        .expect("ProgramInfoLog not valid utf8")
+                );
+            } else {
+                println!("Shader linking successful: {}", id);
+            }
+        }
+    }
 }
